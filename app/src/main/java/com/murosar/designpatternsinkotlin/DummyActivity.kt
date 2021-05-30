ab891070f6ae8503534c88789754dab87dc5a7ab
Mauro Sarti <mauro.sarti@globant.com>
2021-05-30 19:07:52 -0300

    Structural: Composite
    Tested in: Samsung S10+
    Signed off by: Mauro Sarti mauro.sarti@globant.com

--- app/src/main/java/com/murosar/designpatternsinkotlin/DummyActivity.kt
+++ app/src/main/java/com/murosar/designpatternsinkotlin/DummyActivity.kt
@@ -1,7 +1,7 @@
 package com.murosar.designpatternsinkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class DummyActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
